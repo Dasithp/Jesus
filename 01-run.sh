3a166b0f9161e0a5914cb0e3e43bb13a7af1d29c
asterling <asterling@my.uri.edu>
2017-02-14 14:04:26 -0500

    added comment hello

--- 01-run.sh
+++ 01-run.sh
@@ -1,5 +1,5 @@
 #1/bin/bash
-
+#hello
 if [ $# -eq 0 ]; then #commmand runs if variable is equal to or greater than 0 
 	echo "Usage: $0 filename" #displays this message
 	exit 1 #error message for above test failing 
